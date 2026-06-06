a79cac3f04804d57c5d12d556d3197a7b27db01d
glaucio <glaucio@glaucio-pc>
2026-06-06 17:07:44 -0300

    seção 6 - entendendo o conceito de System (Method) Under Test

---
+++ src/main/java/br/com/erudio/business/CourseBussines.java
@@ -0,0 +1,28 @@
+package br.com.erudio.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.erudio.service.CourseService;
+
+// CourseBussines = SUT - System (Method) Under Test
+public class CourseBussines {
+	private CourseService service;
+
+	public CourseBussines(CourseService service) {
+		this.service = service;
+	}
+	
+	public List<String> retrieveCoursesRelatedToSpring(String student) {
+		var filteredCourses = new ArrayList<String>();
+		var allCourses = service.retrieveCourses(student);
+		
+		for (String course : allCourses) {
+			if(course.contains("Spring")) {
+				filteredCourses.add(course);
+			}
+		}
+		
+		return filteredCourses;
+	}
+}
---
+++ src/main/java/br/com/erudio/service/CourseService.java
@@ -0,0 +1,7 @@
+package br.com.erudio.service;
+
+import java.util.List;
+
+public interface CourseService {
+	public List<String> retrieveCourses(String student);
+}
